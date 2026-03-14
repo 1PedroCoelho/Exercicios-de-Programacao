8e11b2566e0e168d229a7584f63f6649a30df0d4
Pedro Coelho <pedrooliveira.ocb@gmail.com>
2026-03-13 23:57:56 -0300

    exercicio 1 da atividade 3

---
+++ Atividade POO 3/Program.cs
@@ -0,0 +1,55 @@
+﻿class ContaBancaria
+{
+    private string titular;
+    private double saldo;
+
+    public string getTitular()
+    {
+        return titular;
+    }
+    public void setTitular(string novoTitular)
+    {
+        titular = novoTitular;
+    }
+    public double getSaldo()
+    {
+        return saldo;
+    }
+    public void setSaldo(double novoSaldo)
+    {
+        saldo = novoSaldo;
+    }
+    public void Depositar(double valor)
+    {
+        saldo += valor;
+        Console.WriteLine($"Valor de R${valor} depositado na sua conta. Seu saldo: {saldo}");
+    }
+    public void Sacar(double valor)
+    {
+        if (saldo < valor)
+        {
+            Console.WriteLine($"Saldo insuficiente para realizar o saque. Seu saldo:{saldo}");
+        }
+        else
+        {
+            Console.WriteLine($"Valor de R${valor} sacado da sua conta. Seu saldo: {saldo}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine();
+        ContaBancaria MinhaConta = new ContaBancaria();
+        MinhaConta.setTitular("Pedro Coelho");
+        Console.WriteLine($"{MinhaConta.getTitular()}");
+        MinhaConta.setSaldo(100);
+        MinhaConta.Depositar(20);
+        MinhaConta.Sacar(50);
+        Console.WriteLine($"Saldo: {MinhaConta.getSaldo()}");
+        
+    }
+}
