4f3461fe5cb752a81bcd7df0c7a34c5c6ca22443
Pedro Coelho <pedrooliveira.ocb@gmail.com>
2026-03-20 10:28:13 -0300

    Exercicio 1 atividade 4

---
+++ Atividade POO 4/Program.cs
@@ -0,0 +1,19 @@
+﻿static class ConversorTemperatura
+{
+    public static double CelsiusParaFahrenheit(double c)
+    {
+      return (c*9/5)+32;  
+    }
+    public static double FahrenheitParaCelsius(double f)
+    {
+        return (f-32)*5/9;
+    } 
+}
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Temperatura em Celsius: {ConversorTemperatura.CelsiusParaFahrenheit(0)}");
+        Console.WriteLine($"Temperatura em Fahrenheit: {ConversorTemperatura.FahrenheitParaCelsius(32)}");
+    }
+}
