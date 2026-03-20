d8ce940c7b68f48867af6e9f1aa42044dfd2c65b
Pedro Coelho <pedrooliveira.ocb@gmail.com>
2026-03-20 10:35:31 -0300

    Exercicio 2 atividade 4

--- Atividade POO 4/Program.cs
+++ Atividade POO 4/Program.cs
@@ -2,18 +2,43 @@
 {
     public static double CelsiusParaFahrenheit(double c)
     {
-      return (c*9/5)+32;  
+        return (c * 9 / 5) + 32;
     }
     public static double FahrenheitParaCelsius(double f)
     {
-        return (f-32)*5/9;
-    } 
+        return (f - 32) * 5 / 9;
+    }
 }
+
+class Aluno
+{
+    public string nome;
+    public static int totalAlunos = 0;
+    public Aluno(string nome)
+    {
+        this.nome = nome;
+        Console.WriteLine($"Aluno {nome} cadastrado");
+        totalAlunos++;
+    }
+    public static void ExibirTotalAlunos()
+    {
+        Console.WriteLine($"Total alunos: {totalAlunos}");
+    }
+}
+
 class Program
 {
     public static void Main()
     {
         Console.WriteLine($"Temperatura em Celsius: {ConversorTemperatura.CelsiusParaFahrenheit(0)}");
         Console.WriteLine($"Temperatura em Fahrenheit: {ConversorTemperatura.FahrenheitParaCelsius(32)}");
+
+        Console.WriteLine("");
+
+        Aluno aluno1 =new Aluno("Pedro");
+        Aluno aluno2 =new Aluno("João");
+        Aluno aluno3 =new Aluno("Sara");
+
+        Aluno.ExibirTotalAlunos();
     }
 }
--- Atividade POO 4/obj/Debug/net9.0/Atividade POO 4.AssemblyInfo.cs
+++ Atividade POO 4/obj/Debug/net9.0/Atividade POO 4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade POO 4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a437e0355c5cba0af9313694523502bda841c635")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3461fe5cb752a81bcd7df0c7a34c5c6ca22443")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade POO 4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade POO 4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
